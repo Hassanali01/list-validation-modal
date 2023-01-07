a4246eea48d1fe48cb7e20ebd158ded117bd2676
Hassan Ali <Hassan.Ali@sagacious.local>
2023-01-07 16:18:45 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import AddUser from './components/users/AddUser';
+import UsersList from './components/users/UsersList';
+
+function App() {
+
+  const [usersList,setUsersList]=useState([])
+  const addUserHandler=(uName,uAge)=>{
+    setUsersList((prevUsers)=>{
+      return [...prevUsers,{name:uName,age:uAge,id:Math.random().toString()}]
+    })
+  }
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList} />
+
+    </div>
+  );
+}
+
+export default App;
